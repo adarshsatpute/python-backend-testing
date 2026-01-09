b864c26009fac61ddbc059f5b58d3780ac6b5ac0
adarshsatpute <127473652+adarshsatpute@users.noreply.github.com>
2026-01-10 01:32:00 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,9 +7,9 @@ CORS(app)
 
 # Database Configuration
 db_config = {
-    'host': 'book-rds.cinsoscgioqa.us-east-1.rds.amazonaws.com',
+    'host': 'database-1.c4zkewuuktd7.us-east-1.rds.amazonaws.com'
     'user': 'admin',
-    'password': 'SuperSecretPass123',
+    'password': 'Alka1234',
     'database': 'dev'
 }
 
